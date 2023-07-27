4b89329816d97e2ad4e1bb81bba47e51dd552b45
Aneta Grochowska <aneta.anette.g3@gmail.com>
2023-07-27 22:13:05 +0200

    almost done

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("dfs");
+let counter = 0;
+document.getElementById("increaseBtn").onclick = function() {
+    counter +=1;
+    document.getElementById("countLabel").innerHTML = counter;
+
+    if (counter >= 32) {
+        document.getElementById("allahuakbar").style.background = "rgba(240, 166, 161, 0.75)";
+    }
+}
+document.getElementById("resetBtn").onclick = function() {
+    document.getElementById("countLabel").innerHTML = 0;
+    document.getElementById("allahuakbar").style.background = "rgba(255, 255, 255, 0.9)";
+}
+
+
+let counter1 = 0;
+document.getElementById("increaseBtn1").onclick = function() {
+    counter1 +=1;
+    document.getElementById("countLabel1").innerHTML = counter1;
+    if (counter1 >= 32) {
+        document.getElementById("alhamdulillah").style.background = "rgba(240, 166, 161, 0.75)";
+    }
+}
+document.getElementById("resetBtn1").onclick = function() {
+    document.getElementById("countLabel1").innerHTML = 0;
+    document.getElementById("alhamdulillah").style.background = "rgba(255, 255, 255, 0.9)";
+}
+
+
+let counter2 = 0;
+document.getElementById("increaseBtn2").onclick = function() {
+    counter2 +=1;
+    document.getElementById("countLabel2").innerHTML = counter2;
+    if (counter2 >= 32) {
+        document.getElementById("subhanaallah").style.background = "rgba(240, 166, 161, 0.75)";
+    }
+}
+document.getElementById("resetBtn2").onclick = function() {
+    document.getElementById("countLabel2").innerHTML = 0;
+    document.getElementById("subhanaallah").style.background = "rgba(255, 255, 255, 0.9)";
+}
